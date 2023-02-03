e55046bbe430a156f7649fefd2cc88a64c168fab
Joel Fernandes <up201904977@fe.up.pt>
2023-02-03 23:41:15 +0000

    feat: initial root back spawn

--- Assets/TipController.cs
+++ Assets/TipController.cs
@@ -7,6 +7,7 @@ public class TipController : MonoBehaviour
 
     public GameObject rootSegment;
 
+    private Vector3 prevPosition = new Vector3();
     private Vector3 direction = new Vector3();
     private Vector3 rotation = new Vector3();
     private float blockWidth = 0.1f;
@@ -28,13 +29,22 @@ public class TipController : MonoBehaviour
             verticalInput = Input.GetAxisRaw("Vertical");
         }
 
-        direction = new Vector3(horizontalInput, verticalInput, 0);
+        direction = new Vector3(horizontalInput, verticalInput);
         rotation = new Vector3(0, 0, 90.0f * horizontalInput + (verticalInput != 1.0f ? 180.0f : 0.0f));
     }
 
     void FixedUpdate()
     {
+        prevPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         transform.position = transform.position + direction * blockWidth;
+
+        if (((int)transform.position.x) != ((int)prevPosition.x)) {
+            Instantiate(rootSegment, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(transform.eulerAngles));
+        }
+
+        if (((int)transform.position.y) != ((int)prevPosition.y)) {
+            Instantiate(rootSegment, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(transform.eulerAngles));
+        }
 
         transform.eulerAngles = rotation;
     }
